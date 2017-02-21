7fa7127885d6928d7d0a98e02071591a8bf24178
Taylor Flatt <taylorjflatt@gmail.com>
2017-02-20 19:26:59 -0600

    Create main.py
    Initial commit.

---
+++ main.py
@@ -0,0 +1,53 @@
+"""Encrypts text using an iterative block version of the Vigenere Cipher"""
+# Created by Taylor Flatt in Python 3.6
+
+TABULA = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ,.-_'
+
+
+def encrypt(text, key):
+    """Encrypts a block of text using a key."""
+
+    cipher = ""
+    for i, j in zip(text, key):
+        text_pos = TABULA.index(i)
+        key_pos = TABULA.index(j)
+        cipher_pos = (text_pos + key_pos) % 30
+        cipher += TABULA[cipher_pos]
+
+    return cipher
+
+
+def main():
+    """Prompts the user for some text and a key. Then passes their text
+    through the encryption method iteratively until it is completely encrypted."""
+
+    plaintext = input('Enter something to encrypt: ')
+    key = input('Enter key: ')
+
+    # Break the cipher into blocks relative to the key length.
+    blocks = []
+    blocks.insert(0, key)
+    i = 0
+    j = len(key)
+    iterations = (len(plaintext) // len(key)) + 1
+
+    # Iterate for as many blocks are necessary for the plaintext input.
+    for index in range(0, iterations):
+        blocks.append(
+            encrypt(plaintext[i:j].upper(), blocks[index][:j - i].upper()))
+        i += len(key)
+
+        # Catches blocks smaller than the key.
+        if j + len(key) > len(plaintext) - 1:
+            j = len(plaintext)
+        else:
+            j += len(key)
+
+    blocks_joined = ''.join(blocks)
+
+    print("Plaintext: ", plaintext)
+    print("Key: ", blocks_joined[:-len(key)])
+    print("Ciphertext: ", blocks_joined)
+
+if __name__ == "__main__":
+    main()
